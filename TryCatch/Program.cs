0717493a5226583b7269f2bf05216fa7d03d12f1
Richard Tanner <ratanner0@gmail.com>
2022-06-29 10:37:41 -0400

    Add files via upload

---
+++ TryCatch/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TryCatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number!");
+            string userInput = Console.ReadLine();
+
+            try
+            {
+                int userInputAsInt = int.Parse(userInput);
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Formnat Exception, please only enter a number!");
+            }
+            finally
+            {
+                Console.WriteLine("This will always be ran");
+            }
+            Console.ReadKey();
+        }
+    }
+}
